23c5f395dae270e7afe5d7c2a94eda1647988bb0
Vesa Laakso <laakso.vesa@gmail.com>
2012-02-16 19:00:57 +0200

    game-kansioon pelin pohja

---
+++ game/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * @fileOverview Kaiken pohja
+ */
+
+/**
+ * @namespace Pelin nimiavaruus
+ */
+var Sergei = {
+
+  /**
+   * Alustus. Kutsutaan heti kun DOM ready.
+   */
+  init: function () {
+    debug.info('Sergei.init() called!');
+  }
+};
